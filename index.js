675715ecd5844501fdcfe6240f031a4ec25045e9
Lumbagu <masiver.ae@yahoo.com>
2024-04-18 15:46:16 +0200

    cleanup and refactor

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
+const cors = require("cors");
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.static("public"));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", (req, res) => {
+    res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on port ${listener.address().port}`);
 });
